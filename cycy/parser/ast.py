1ee752562b15d8e6d1aa2fd7663460ae2383270c
Julian Berman <Julian@GrayVines.com>
2015-03-25 14:17:23 -0400

    AST base class

---
+++ cycy/parser/ast.py
@@ -0,0 +1,9 @@
+class Node(object):
+    def __eq__(self, other):
+        return (
+            self.__class__ == other.__class__ and
+            self.__dict__ == other.__dict__
+        )
+
+    def __ne__(self, other):
+        return not self == other
