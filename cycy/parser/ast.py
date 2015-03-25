4a65f7010e901b5554db13688416a20d8bc66e0c
Julian Berman <Julian@GrayVines.com>
2015-03-25 15:05:55 -0400

    Characteristic reprs for nodes.

--- cycy/parser/ast.py
+++ cycy/parser/ast.py
@@ -1,22 +1,21 @@
+from characteristic import Attribute, attributes
+
+
 class Node(object):
-    def __eq__(self, other):
-        return (
-            self.__class__ == other.__class__ and
-            self.__dict__ == other.__dict__
-        )
+    pass
 
-    def __ne__(self, other):
-        return not self == other
 
+@attributes([
+    Attribute(name="operand"),
+    Attribute(name="left"),
+    Attribute(name="right"),
+])
 class BinaryOperation(Node):
-    def __init__(self, operand, left, right):
-        assert operand in ("+", "-") # for now
-        self.operand = operand
-        self.left = left
-        self.right = right
+    def __init__(self):
+        assert self.operand in ("+", "-") # for now
 
+@attributes([Attribute(name="value")])
 class Int32(Node):
-    def __init__(self, value):
-        assert isinstance(value, int)
-        assert -2**32 < value <= 2**32-1
-        self.value = value
+    def __init__(self):
+        assert isinstance(self.value, int)
+        assert -2**32 < self.value <= 2**32-1
