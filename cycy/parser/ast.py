6381204585c64ed70bf23237731bdb92db445c05
Julian Berman <Julian@GrayVines.com>
2015-03-25 14:40:13 -0400

    Add the basic AST nodes.

--- cycy/parser/ast.py
+++ cycy/parser/ast.py
@@ -7,3 +7,16 @@ class Node(object):
 
     def __ne__(self, other):
         return not self == other
+
+class BinaryOperation(Node):
+    def __init__(self, operand, left, right):
+        assert operand in ("+", "-") # for now
+        self.operand = operand
+        self.left = left
+        self.right = right
+
+class Int32(Node):
+    def __init__(self, value):
+        assert isinstance(value, int)
+        assert -2**32 < value <= 2**32-1
+        self.value = value
