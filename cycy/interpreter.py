2b253e9e3cabcfb58d9cdb30ed71c1696103aa17
tompko <tompko@gmail.com>
2015-03-26 11:53:10 -0400

    Add jit scaffolding

--- cycy/interpreter.py
+++ cycy/interpreter.py
@@ -4,6 +4,22 @@ from cycy import bytecode, compiler
 from cycy.objects import W_Char
 from cycy.parser.sourceparser import parse
 
+# So that you can still run this module under standard CPython, I add this
+# import guard that creates a dummy class instead.
+try:
+    from rpython.rlib.jit import JitDriver, purefunction
+except ImportError:
+    class JitDriver(object):
+        def __init__(self,**kw): pass
+        def jit_merge_point(self,**kw): pass
+        def can_enter_jit(self,**kw): pass
+    def purefunction(f): return f
+
+def get_location(pc):
+    return "%s" % pc
+
+jitdriver = JitDriver(greens=['pc'], reds=['byte_code'],
+        get_printable_location=get_location)
 
 class CyCy(object):
     """
@@ -13,6 +29,8 @@ class CyCy(object):
     def run(self, byte_code):
         pc = 0
         while pc < len(byte_code.instructions):
+            jitdriver.jit_merge_point(pc=pc, byte_code=byte_code)
+
             opcode = byte_code.instructions[pc]
             arg = byte_code.instructions[pc + 1]
             pc += 2
