1fa4a509b90f3a3da4192a8d07b7594580bfdeac
Julian Berman <Julian@GrayVines.com>
2015-03-26 15:25:47 -0400

    Hey look a E (that's a REPL, without Reading, Printing, or Looping)

--- cycy/target.py
+++ cycy/target.py
@@ -9,16 +9,20 @@ from rpython.rlib.streamio import open_file_as_stream
 from rpython.jit.codewriter.policy import JitPolicy
 
 from cycy.interpreter import interpret
+from cycy.repl import REPL
 
 
 def main(argv):
-    if len(argv) != 2:
+    if len(argv) == 1:
+        REPL().run()
+    elif len(argv) != 2:
         print __doc__
         return 1
-    source_file = open_file_as_stream(argv[1])
-    data = source_file.readall()
-    source_file.close()
-    interpret(data)
+    else:
+        source_file = open_file_as_stream(argv[1])
+        data = source_file.readall()
+        source_file.close()
+        interpret(data)
     return 0
 
 
